0b80945748db64a955312f24325e7e4e3cbddc9e
Cesar Rodriguez <cesar@ooyala.com>
2014-04-09 16:38:09 -0500

    Change Properties to Fields

--- lib/xml2go.rb
+++ lib/xml2go.rb
@@ -38,10 +38,10 @@ module Xml2Go
 
   # class representing a Go struct
   class Struct
-    attr_accessor :properties, :name
+    attr_accessor :fields, :name
 
     # represents member variables
-    class Property
+    class Field
       attr_accessor :type, :name, :xml_tag
 
       def initialize(name, type, xml_tag)
@@ -59,28 +59,28 @@ module Xml2Go
     def initialize(name)
       # @name, _ = Xml2Go::singularize(name)
       @name = name
-      @properties = {}
+      @fields = {}
     end
 
     # adds member variable to struct
     def add_property(var_name, type, xml_tag)
       # struct already has that property, consider it an array
-      if @properties.has_key?(var_name) && !@properties[var_name].type.include?("[]") then
-        @properties[var_name].type = "[]" + @properties[var_name].type
+      if @fields.has_key?(var_name) && !@fields[var_name].type.include?("[]") then
+        @fields[var_name].type = "[]" + @fields[var_name].type
 
         if Xml2Go::config[:plural_arrays] then
-          @properties[var_name].name << "s" if @properties[var_name].name[-1] != "s"
+          @fields[var_name].name << "s" if @fields[var_name].name[-1] != "s"
         end
 
       else
-        @properties[var_name] = Property.new(var_name, type, xml_tag)
+        @fields[var_name] = Field.new(var_name, type, xml_tag)
       end
     end
 
     def to_s
-      properties_string = properties.values.join("\n")
+      fields_string = fields.values.join("\n")
       "type #{@name} struct {\n
-      #{properties_string}
+      #{fields_string}
       }
       "
     end
@@ -155,7 +155,7 @@ module Xml2Go
 
     struct = Struct.new(struct_name)
 
-    # add attributes as properties
+    # add attributes as fields
     if @@config[:add_attrs] then
       add_attrs_to_struct(element, struct)
     end
